b4719a7d3f72f251c5e37c8696c07e971f1baab5
SABERGLOW <wali6985@gmail.com>
2022-09-25 15:56:10 +0200

    :construction: Wrap MyApp with SessionProvider
    Wrapping MyApp with SessionProvider to allow us to use session and hooks from next-auth inside our app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+/**
+ * Wrapping "MyApp" with "SessionProvider" will make session available to all pages 
+ * and components in the application without the need to use the getSession in getServerSideProps or getStaticProps.
+ * This allows us to use the session and hooks from next-auth/react inside our _app.tsx.
+*/
+
+
+/**
+ * MyApp is a function that takes in a Component and pageProps, and returns a SessionProvider component
+ * that takes in a session prop and a Component component that takes in pageProps
+ * @param {AppProps}  - AppProps
+ * @returns A higher order component that wraps the component that is being rendered.
+ */
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+  /* A higher order component that wraps the component that is being rendered. */
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
