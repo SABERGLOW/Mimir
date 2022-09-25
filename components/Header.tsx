b6a55e94215ec89b249b2e325061ed2ecbb7b953
SABERGLOW <wali6985@gmail.com>
2022-09-25 17:07:08 +0200

    :sparkles: Implement SignIn and SignOut with Reddit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import {
 	ChevronDownIcon,
@@ -9,7 +9,7 @@ import {
 	Bars3Icon,
 } from "@heroicons/react/24/solid";
 
-import { 	
+import {
 	ArrowUpRightIcon,
 	BellIcon,
 	ChatBubbleOvalLeftEllipsisIcon,
@@ -20,8 +20,9 @@ import {
 	UserCircleIcon,
 } from "@heroicons/react/24/outline";
 
-
 function Header() {
+	/* A function that checks if the user is logged in or not. */
+	const { data: session, status } = useSession();
 	return (
 		//... This is the Header component, it contains the navigation bar and the logo ...//
 		<div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
@@ -57,7 +58,6 @@ function Header() {
 				</p>
 				<ChevronDownIcon className='h-5 w-5' />
 			</div>
-
 			{/* Search Bar */}
 			<form className='flex flex-1 items-center space-x-2 border border-gray-200 rounded bg-gray-100 px-3 py-1 '>
 				{/* flex : put the items in a row,
@@ -80,9 +80,8 @@ function Header() {
 				/>
 				<button hidden type='submit' />
 			</form>
-
 			{/* Right Header Side: Profile + Messages + Notifications etc */}
-			<div className="text-gray-500 space-x-2 items-center mx-5 hidden lg:inline-flex ">
+			<div className='text-gray-500 space-x-2 items-center mx-5 hidden lg:inline-flex '>
 				{/* text-gray-500 : icon color set to gray
 					items-center : center along the y axis,
 					space-x-2 : horizontal space of 2 between the elements
@@ -91,68 +90,70 @@ function Header() {
 					lg:inline-flex : show the icons on large screens
 				*/}
 
-				<ArrowUpRightIcon className="icon hidden lg:inline-flex "/>
-				<ChartBarSquareIcon className="icon hidden lg:inline-flex "/>
-				<VideoCameraIcon className="icon hidden lg:inline-flex "/>
+				<ArrowUpRightIcon className='icon hidden lg:inline-flex ' />
+				<ChartBarSquareIcon className='icon hidden lg:inline-flex ' />
+				<VideoCameraIcon className='icon hidden lg:inline-flex ' />
 
 				{/* A horizontal line that separates the icons on the right side of the header. */}
-				<hr className="h-10 border border-gray-100 hidden lg:inline-flex "/>
+				<hr className='h-10 border border-gray-100 hidden lg:inline-flex ' />
 
-				<ChatBubbleOvalLeftEllipsisIcon className="icon hidden lg:inline-flex "/>
-				<BellIcon className="icon hidden lg:inline-flex "/>
-				<PlusIcon className="icon hidden lg:inline-flex "/>
-				<MegaphoneIcon className="icon hidden lg:inline-flex "/>
+				<ChatBubbleOvalLeftEllipsisIcon className='icon hidden lg:inline-flex ' />
+				<BellIcon className='icon hidden lg:inline-flex ' />
+				<PlusIcon className='icon hidden lg:inline-flex ' />
+				<MegaphoneIcon className='icon hidden lg:inline-flex ' />
 			</div>
-
-			<div className="ml-5 flex items-center lg:hidden">
-				{/* Hamburger Menu Icon */}
-				<Bars3Icon className="icon">
-				</Bars3Icon>
+			{/* Hamburger Menu Icon */}
+			<div className='ml-5 flex items-center lg:hidden'>
+				<Bars3Icon className='icon'></Bars3Icon>
 			</div>
+			{/* Mimir Logo and Sign In / Sign Out button */}
+			{/* A ternary operator. It is a shorthand way of writing an if-else statement. */}
+			{session ? (
+				<div
+					/* A function that calls the signOut function from the next-auth library. */
+					onClick={() => signOut()}
+					className='hidden lg:flex items-center cursor-pointer space-x-2 border border-gray-200 bg-gray-100 rounded p-2'
+				>
+					<div className='relative h-7 w-20 flex-shrink-0'>
+						<Image
+							objectFit='contain'
+							src='/logo/LogoText.svg'
+							layout='fill'
+							alt='login'
+						/>
+						{/* objectFit='contain': make sure the image fits in the container */}
+					</div>
+					
+					<div className="flex-1 text-xs">
+						<p className="truncate">{session?.user?.name}</p>
+						<p className='text-gray-400'>7 Mims</p>
+					</div>
+					
+					<ChevronDownIcon className="flex-shrink-0 h-5 text-gray-400" />
 
-			{/* Mimir Logo and Sign In button */}
-			<div 
-			/* A function that calls the signIn function from the next-auth library. */
-			onClick={() => signIn()}
-			className="hidden lg:flex items-center cursor-pointer space-x-2 border border-gray-200 bg-gray-100 rounded p-2">
-				{/* hidden : hide the logo and sign in button on small screens
-					lg:flex : show the logo and sign in button on large screens
-					items-center : center along the y axis,
-					cursor-pointer : change the cursor to a pointer when we hover over the component
-					space-x-2 : horizontal space of 2 between the elements
-					border : add a border of width 1px
-					border-gray-200 : color of the border
-					bg-gray-100 : background color
-					rounded : round the corners, 4px
-					p-2 : padding of 2
-				*/}
-
-				<div className="relative h-7 w-20 flex-shrink-0">
-				<Image
-					objectFit='contain' 
-					src='/logo/LogoText.svg'
-					layout='fill'
-					alt='login'
-				/>
-				{/* objectFit='contain': make sure the image fits in the container */}
 				</div>
+			) : (
+				<div
+					/* A function that calls the signIn function from the next-auth library. */
+					onClick={() => signIn()}
+					className='hidden lg:flex items-center cursor-pointer space-x-2 border border-gray-200 bg-gray-100 rounded p-2'
+				>
+					<div className='relative h-7 w-20 flex-shrink-0'>
+						<Image
+							objectFit='contain'
+							src='/logo/LogoText.svg'
+							layout='fill'
+							alt='login'
+						/>
+						{/* objectFit='contain': make sure the image fits in the container */}
+					</div>
 
-				<p className="text-gray-400">
-					Log In
-				</p>
-			</div>
-
-			<div className="mobile-menu hidden">
-				{/* Mobile Menu */}
-			</div>
-			
+					<p className='text-gray-400'>Log In</p>
+				</div>
+			)}
+			<div className='mobile-menu hidden'>{/* Mobile Menu */}</div>
 		</div>
-		
-
 	);
-
-
-	
 }
 
 export default Header;
