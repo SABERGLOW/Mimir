1a825596381c8797237fdf93e9d15e19ed4d876e
SABERGLOW <wali6985@gmail.com>
2022-09-29 14:26:45 +0200

    🗃️ Add insertSUBREDDIT mutation

--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-{/* A GraphQL mutation is being called. */}
+{/* A GraphQL mutation is being called that will Inset a Post in the Supabase DB */}
 export const ADD_POST = gql`
     mutation MyMutation(
         $title: String!
@@ -23,6 +23,17 @@ export const ADD_POST = gql`
             image
             subreddit_id
             username
+        }
+    }
+`
+
+/* A GraphQL mutation that will insert a Subreddit in the Supabase DB. */
+export const ADD_SUBREDDIT = gql`
+    mutation MyMutation($topic: String!){
+        insertSUBREDDIT(topic: $topic){
+            id
+            topic
+            created_at
         }
     }
 `
