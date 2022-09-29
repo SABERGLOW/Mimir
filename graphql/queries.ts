0d8d00ee9735ccdb13c14ba0e2c02ec8002e30d0
SABERGLOW <wali6985@gmail.com>
2022-09-29 14:28:03 +0200

    🗃️ Add getSubredditListByTopic to queries.ts

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,1 +1,12 @@
 import { gql } from "@apollo/client";
+
+/* A GraphQL query which gets subreddit list by topics */
+export const GET_SUBREDDIT_LIST_BY_TOPIC = gql`
+    query MyQuery($topic: String!) {
+        getSubredditListByTopic(topic: $topic) {
+            id
+            topic
+            created_at
+        }
+    }
+`; 
