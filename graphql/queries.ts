46e80935b19abb68979624e8d83ade0e1261f7d2
SABERGLOW <wali6985@gmail.com>
2022-10-02 15:15:32 +0200

    🗃️ Update Materialized queries, Add GET_ALL_POSTS query
    Update Materialized queries in index.graphql to put all data related to posts, and add GET_ALL_POSTS query to queries.ts

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,40 @@
 import { gql } from "@apollo/client";
 
+/* A GraphQL query which gets all posts. comments, subreddit, and votes related to that post will also be pulled in using Materializer */
+export const GET_ALL_POSTS = gql`
+    query MyQuery{
+        getPOSTList{
+            created_at
+            id
+            subreddit_id
+            username
+            title
+            body
+            image
+            comments{	
+                created_at
+                id
+                post_id
+                username
+                text
+            }
+            subreddit{
+                created_at
+                id
+                topic
+            }
+            votes{
+                created_at
+                id
+                post_id
+                username
+                upvote
+            }
+        }
+    }
+`;
+
+
 /* A GraphQL query which gets subreddit list by topics */
 export const GET_SUBREDDIT_LIST_BY_TOPIC = gql`
     query MyQuery($topic: String!) {
