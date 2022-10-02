7561b85a483f6126c077d81bc46662cc86836a81
SABERGLOW <wali6985@gmail.com>
2022-10-02 15:16:48 +0200

    🚧 Add feed component, WIP
    Feed component shall contain our posts, comments, votes etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
@@ -22,7 +23,7 @@ const Home: NextPage = () => {
 
       {/* Feed */}
       <div className="flex">
-
+        <Feed/>
       </div>
 
     </div>
