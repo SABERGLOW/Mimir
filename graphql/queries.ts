5af6748bdc3a54c57d94b33644aac752b1627e57
SABERGLOW <wali6985@gmail.com>
2022-10-04 21:53:27 +0200

    🗃️  Update queries.tsx: Add GET_POST_BY_POST_ID query to get post

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,39 @@
 import { gql } from "@apollo/client";
 
+export const GET_POST_BY_POST_ID = gql`
+    query MyQuery($post_id: ID!) {
+        getPOST(post_id: $post_id) {
+            created_at
+            id
+            subreddit_id
+            username
+            title
+            body
+            image
+            comments{	
+                created_at
+                id
+                post_id
+                username
+                text
+            }
+            subreddit{
+                created_at
+                id
+                topic
+            }
+            votes{
+                created_at
+                id
+                post_id
+                username
+                upvote
+            }
+        }
+    }
+`;
+
+
 /* A GraphQL query which gets all posts. comments, subreddit, and votes related to that post will also be pulled in using Materializer */
 export const GET_ALL_POSTS = gql`
     query MyQuery{
