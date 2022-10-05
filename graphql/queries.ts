04a022523343b320e4e3320e2a67e1258ff24b25
SABERGLOW <wali6985@gmail.com>
2022-10-05 20:27:23 +0200

    🗃️ Update queries.tsx: Add "GET_ALL_POSTS_BY_TOPIC" query
    A GraphQL query which gets all posts by topic. comments, subreddit, and votes related to that post will also be pulled in using Materializer.

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -69,6 +69,40 @@ export const GET_ALL_POSTS = gql`
     }
 `;
 
+/* A GraphQL query which gets all posts by topic. comments, subreddit, and votes related to that post will also be pulled in using Materializer. */
+export const GET_ALL_POSTS_BY_TOPIC = gql`
+    query MyQuery($topic: String!) {
+        getPOSTListByTopic(topic: $topic) {
+            created_at
+            id
+            subreddit_id
+            username
+            title
+            body
+            image
+            comments{	
+                created_at
+                id
+                post_id
+                username
+                text
+            }
+            subreddit{
+                created_at
+                id
+                topic
+            }
+            votes{
+                created_at
+                id
+                post_id
+                username
+                upvote
+            }
+        }
+    }
+`
+
 
 /* A GraphQL query which gets subreddit list by topics */
 export const GET_SUBREDDIT_LIST_BY_TOPIC = gql`
