7f5202f377d56f1ab9daf4d90d728b39d0a1072b
SABERGLOW <wali6985@gmail.com>
2022-10-11 18:53:41 +0200

    🔧 Update tailwind.config: Add Gilroy font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class', // or 'media' or 'class'
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				gilroy: ["Gilroy", "sans-serif"],
+			},
 			animation: {
 				enter: "enter .2s ease-out",
 				leave: "leave .15s ease-in forwards",
