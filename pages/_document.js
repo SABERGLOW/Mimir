0946297b5e3c05ca7686596b46cd821c472f75e3
SABERGLOW <wali6985@gmail.com>
2022-10-18 19:53:24 +0200

    🔍️ Improve SEO: Add lang attribute to Head
    Added a new file: _document.js with <Html lang='en'

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html lang='en'>
+			{" "}
+			{/* Add whichever language you want here */}
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
