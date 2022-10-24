d4fb469849c31ddce0d2a9f9684c7b46fdb4263e
SABERGLOW <wali6985@gmail.com>
2022-10-24 22:12:02 +0200

    🎨 Add div classes

--- components/TopCommunities.tsx
+++ components/TopCommunities.tsx
@@ -19,14 +19,14 @@ function TopCommunities({ index, topic }: Props) {
 		<div className="flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b font-gilroy subpixel-antialiased dark:bg-[#044A42] dark:border-[#3A9188] backdrop-blur-sm">
 			<p className="dark:text-[#B8E1DD]">{index + 1}</p>
 
-			<ChevronUpDownIcon className="h-4 w-4 flex-shrink-0 text-gray-400 dark:text-[#3A9188]" />
+			<ChevronUpDownIcon className="ChevronUpDownIcon h-4 w-4 flex-shrink-0 text-gray-400 dark:text-[#3A9188]" />
 
 			<AvatarSubreddit seed={`/subreddit/${topic}`} />
 
 			<p className="flex-1 truncate text-sm dark:text-[#B8E1DD]">m/{topic}</p>
 
 			<Link href={`/subreddit/${topic}`}>
-				<div className="cursor-pointer rounded-full bg-gray-400 px-3 text-white hover:bg-blue-400 dark:text-[#B8E1DD] dark:bg-[#062824] drop-shadow-[0_10px_10px_rgba(184,225,221,0.05)] hover:drop-shadow-xl dark:shadow-lg">
+				<div className="viewButton cursor-pointer rounded-full bg-gray-400 px-3 text-white hover:bg-blue-400 dark:text-[#B8E1DD] dark:bg-[#062824] drop-shadow-[0_10px_10px_rgba(184,225,221,0.05)] hover:drop-shadow-xl dark:shadow-lg">
 					View
 				</div>
 			</Link>
